b24eaea7e7744341b38be759264cbfe6ec648ce1
Robinhood Shrestha <rawbinnn@gmail.com>
2020-08-23 17:35:13 +0545

    initial commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+  } from "react-router-dom";
+
+
+export default function Navbar() {
+    return (
+        <Router>
+            <nav className="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+                <div className="container">
+                <a className="navbar-brand" href="index.html">Start Bootstrap</a>
+                <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                    Menu
+                    <i className="fas fa-bars"></i>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarResponsive">
+                    <ul className="navbar-nav ml-auto">
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/">Home</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/about-us">About Us</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/news">News</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/contact-us">Contact Us</Link>
+                    </li>
+                    </ul>
+                </div>
+                </div>
+            </nav>
+        </Router>
+    );
+}
