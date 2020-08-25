a874341060e017e544fad2d0db7525a6e30f4abb
Robinhood Shrestha <rawbinnn@gmail.com>
2020-08-25 21:30:16 +0545

    updated

--- src/components/home/PostList.js
+++ src/components/home/PostList.js
@@ -1,41 +1,63 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import Loading from '../../partials/loading/Loading';
 
 const PostList = () => {
     const [posts, setPosts] = useState();
+    const [loading, setLoading] = useState(true);
+    const [empty, setEmpty] = useState(false);
 
     const getPosts = async () => {
-        const response = await axios.get('http://larapress.seshra.com/api/posts?limit=10');
-        setPosts(response.data.data);
+        const response = await axios.get('http://larapress.seshra.com/api/posts?limit=10&page=1');
+        if(response.data.status) {
+            setPosts(response.data.data.items);
+        }else{
+            setEmpty(true);
+        }
+        setLoading(false);
     };
     useEffect(() => {
         getPosts();
     }, []);
-    return (
-        <div>
-            {posts && posts.map(post => (
-                <div>
-                    <div class="post-preview">
-                    <a href={"/post/"+post.id}>
-                        <h2 class="post-title">
-                        {post.title}
-                        </h2>
-                        <h3 class="post-subtitle">
-                        {post.content}
-                        </h3>
-                    </a>
-                    <p class="post-meta">Posted by 
-                        <a href="#">{post.author.name}</a> 
-                        on {post.post_date}</p>
-                    </div>
-                    <hr />
-                </div>
-            ))}
-            <div class="clearfix">
-                <a class="btn btn-primary float-right" href="#">Older Posts →</a>
+    if(loading) {
+        return (
+            <Loading />
+        )
+    }
+    else if(empty){
+        return (
+            <div>
+                <p>No data available...</p>
             </div>
-        </div>
-    )
+        )
+    }
+    else{
+        return (
+            <div>
+                {posts && posts.map(post => (
+                    <div>
+                        <div class="post-preview">
+                        <a href={"/post/"+post.id}>
+                            <h2 class="post-title">
+                            {post.title}
+                            </h2>
+                            <h3 class="post-subtitle">
+                            {post.content}
+                            </h3>
+                        </a>
+                        <p class="post-meta">Posted by 
+                            <a href="#">{post.author.name}</a> 
+                            on {post.post_date}</p>
+                        </div>
+                        <hr />
+                    </div>
+                ))}
+                <div class="clearfix">
+                    <a class="btn btn-primary float-right" href="#">Older Posts →</a>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default PostList
--- src/components/single_post/Post.js
+++ src/components/single_post/Post.js
@@ -1,25 +1,35 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useParams } from "react-router-dom";
+import Loading from '../../partials/loading/Loading';
 
 const Post = () => {
     const [post, setPost] = useState();
+    const [loading, setLoading] = useState(true);
     let { id } = useParams();
 
     const getPost = async () => {
         const response = await axios.get('http://larapress.seshra.com/api/post?post_id='+id);
         setPost(response.data.data);
+        setLoading(false)
     };
     useEffect(() => {
         getPost();
     }, []);
-    return (
-        <div>
-            {post &&
-                post.content
-            }
-        </div>
-    )
+    if(loading) {
+        return (
+            <Loading />
+        )
+    }
+    else{
+        return (
+            <div>
+                {post &&
+                    post.content
+                }
+            </div>
+        )
+    }
 }
 
 export default Post
