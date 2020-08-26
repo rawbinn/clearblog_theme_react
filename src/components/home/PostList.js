3efac39de4622f769b3c16c867a85e75c179f3c8
Robinhood Shrestha <rawbinnn@gmail.com>
2020-08-26 23:19:53 +0545

    pagination added

--- src/components/home/PostList.js
+++ src/components/home/PostList.js
@@ -1,24 +1,69 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
 import axios from 'axios';
 import Loading from '../../partials/loading/Loading';
+import Pagination from 'reactjs-hooks-pagination';
+
+const pageLimit = 5;
+const initialState = {  
+  user: {},  
+  loading: true,  
+  error: ''  
+}  
+ 
+const Reducer = (state, action) => {  
+  switch (action.type) {  
+      case 'OnSuccess':  
+          return {  
+              loading: false,  
+              user: action.payload,  
+              error: ''  
+          }  
+      case 'OnFailure':  
+          return {  
+              loading: false,  
+              user: {},  
+              error: 'Something went wrong'  
+          }  
+ 
+      default:  
+          return state  
+  }  
+}
 
 const PostList = () => {
     const [posts, setPosts] = useState();
     const [loading, setLoading] = useState(true);
     const [empty, setEmpty] = useState(false);
+    const [state, dispatch] = useReducer(Reducer, initialState);
+  const [offset, setOffset] = useState(0);
+  const [totalRecords, setTotalRecords] = useState(50);
+  const [currentPage,setCurrentPage] = useState(1);
 
     const getPosts = async () => {
-        const response = await axios.get('http://larapress.seshra.com/api/posts?limit=10&page=1');
-        if(response.data.status) {
-            setPosts(response.data.data.items);
-        }else{
-            setEmpty(true);
-        }
+        const response = await axios.get('http://larapress.seshra.com/api/posts?limit='+pageLimit+'&page='+currentPage)
+        .then(response => {  
+            dispatch({ type: 'OnSuccess', payload: response.data })  
+            if(response.data.status) {
+                setTotalRecords(response.data.data.totalRecords);
+                setPosts(response.data.data.items);
+                
+            }else{
+                setEmpty(true);
+            }
+        })  
+        .catch(error => {  
+            dispatch({ type: 'OnFailure' })  
+        })  ;
+       
         setLoading(false);
     };
     useEffect(() => {
         getPosts();
-    }, []);
+    }, [currentPage]);
+
+    const {user,error}  =state;
+
+
     if(loading) {
         return (
             <Loading />
@@ -53,7 +98,12 @@ const PostList = () => {
                     </div>
                 ))}
                 <div class="clearfix">
-                    <a class="btn btn-primary float-right" href="#">Older Posts →</a>
+                         <Pagination
+                totalRecords={totalRecords}
+                pageLimit={pageLimit}
+                pageRangeDisplayed={1}
+                onChangePage={setCurrentPage}
+      />
                 </div>
             </div>
         )
